c43f2b496b56b155f42760b15f7188d91eb6f41a
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-14 15:30:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,13 @@
 def create_an_empty_array
- [] 
+  
 end
 
 def create_an_array
-  ["melissa", "allison", "gabriel", "jinett"]
+ 
 end
 
-def add_element_to_end_of_array(array, element)<<"mel"
+def add_element_to_end_of_array(array, element)
+  
 end
 
 def add_element_to_start_of_array(array, element)
